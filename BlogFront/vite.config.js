439d8e42c442be17ccb29dcfb240846c7f5a7330
Akshat20042 <makshat.2004@gmail.com>
2024-06-29 03:15:42 +0530

    vite config

--- BlogFront/vite.config.js
+++ BlogFront/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    build: {
+      outDir: "public",
+    },
     proxy: {
       "/api": {
         target: "http://localhost:3000",
